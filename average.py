6b2e0b912ac4c82194bba4ee1d1e32159177706e
Talha Jamal <talhajamal11@gmail.com>
2019-07-25 11:50:10 +0500

    Divide Function

--- average.py
+++ average.py
@@ -7,6 +7,9 @@ round3 = 50
 # Calculate the average
 average = (round1 + round2 + round3) / 3
 
+def divide(round1, round2, round3):
+    return (round1/round2)/round3
+
 # Print out the test score
 print ("the average score is: "), average 
 print ("Andy")
