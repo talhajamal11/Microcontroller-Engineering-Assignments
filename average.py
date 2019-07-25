c2ecec847cdc5c3a7dd2a7a0188db92c2f130043
Talha Jamal <talhajamal11@gmail.com>
2019-07-25 11:32:31 +0500

    Change to Average File

--- average.py
+++ average.py
@@ -1,12 +1,12 @@
 # Get three test score
-round1 = int(raw_input("Enter score for round 1: "))
+round1 = 30
 
-round2 = int(raw_input("Enter score for round 2: "))
-
-round3 = int(raw_input("Enter score for round 3: "))
+round2 = 40
+round3 = 50
    
 # Calculate the average
 average = (round1 + round2 + round3) / 3
 
 # Print out the test score
-print "the average score is: ", average 
+print ("the average score is: "), average 
+print ("Andy")
